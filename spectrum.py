b15875d0392185994207032ed2744e869e973ca0
muke <muke@debian>
2019-02-16 17:13:07 +0000

    first working example with pyplot to pin down core idea.

--- spectrum.py
+++ spectrum.py
@@ -6,28 +6,30 @@ import matplotlib.pyplot as plt
 
 class spectrum():
 	def __init__(self):
-		self.samples, self.audio = read("test.wav")
-		self.frame = 1
+		self.samples, self.audio = read("/mnt/New_Volume/music/Aphex Twin/SAW1/01 Xtal.wav")
+		self.frame = 0
 		self.bufferSize = 1024
 		self.overlap = 0
+		self.audio = np.mean(self.audio, axis=1) #average channels
 		self.audioBuffer = np.zeros(1024, dtype=float)
 		self.plotting()
 
 	def fillBuffer(self): #TODO: window audio
 		self.audioBuffer = np.zeros(1024, dtype=float) #flush out buffer
 		frameSize = (self.frame*self.bufferSize)-self.overlap #keep track of how  many frames in each iteration
-		for i in range(frameSize, frameSize+bufferSize+1):
-			self.audioBuffer.append(self.audio[i])
-			if self.audio[i] == self.audio[-1]: #if at end of file before buffer fills
+		for c, i in enumerate(range(frameSize, frameSize+self.bufferSize)):
+			try:
+				print(i,c)
+				self.audioBuffer[c] = self.audio[i]
+			except IndexError:
 				return 1
 		self.overlap = 100 #feels hacky lol
-		self.frames+=1
+		self.frame+=1
 		return 0
 
 	def FFT(self):
-		self.audioBuffer = np.mean(self.audioBuffer, axis=1) #average channels
-		power = np.abs(np.ftt.ftt(self.audioBuffer)) #abs calculates the complex magnitude, for some reason
-		freqs = np.ftt.fttfreq(self.bufferSize, 1/self.samples)
+		power = np.abs(np.fft.fft(self.audioBuffer)) #abs calculates the complex magnitude, for some reason
+		freqs = np.fft.fftfreq(self.bufferSize, 1/self.samples)
 		return freqs, power
 
 	def plotting(self):
@@ -37,4 +39,4 @@ class spectrum():
 			plt.show()
 
 
-
+spectrum()
