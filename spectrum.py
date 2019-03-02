5e53f9d69fe1125cb04148ebd9a341adb14ba58a
muke <muke@debian>
2019-03-02 18:22:04 +0000

    added frequency windowing

--- spectrum.py
+++ spectrum.py
@@ -5,13 +5,13 @@
 from scipy.io.wavfile import read
 import numpy as np 
 import matplotlib.pyplot as plt 
-
+#try and implement as many of these numpy functions yourself as you can tbh
 
 class spectrum():
 	def __init__(self): #TODO: see if you can replace the scipy wav read function
 		self.sampleRate, self.audio = read("/mnt/New_Volume/music/Aphex Twin/SAW1/01 Xtal.wav")
 		print(self.sampleRate)
-		self.frame = 0
+		self.slice = 0
 		self.bufferSize = 1024 
 		self.overlap = 0
 		print(len(self.audio))
@@ -20,20 +20,21 @@ class spectrum():
 		self.FFT()
 		self.plotting()
 
-	def fillBuffer(self): #TODO: window audio, convert power to dB
+	def fillBuffer(self):
 		self.audioBuffer = np.zeros(1024, dtype=float) #flush out buffer
-		frameSize = (self.frame*self.bufferSize)-self.overlap #keep track of how  many frames in each iteration
+		frameSize = (self.slice*self.bufferSize)-self.overlap #keep track of how  many frames in each iteration
 		for c, i in enumerate(range(frameSize, frameSize+self.bufferSize)):
 			try:
 				self.audioBuffer[c] = self.audio[i]
-			except IndexError:
+			except IndexError: #towards end of audio data, last point probably won't fall on 1024 exactly. Need to break out of loop with whatever data is there
 				return 1
-		self.overlap = 100 #first frame isn't incorrectly cut back by 100 bytes (feels hacky lol)
-		self.frame+=1
+		self.overlap = 100 #first slice isn't incorrectly cut back by 100 bytes (feels hacky lol)
+		self.slice+=1
 		return 0
 
-	def FFT(self):
-		power = np.abs(np.fft.fft(self.audioBuffer)) #abs calculates the complex magnitude, for some reason
+	def FFT(self): #TODO: convert power to dB
+		win = np.kaiser(self.bufferSize, 7)
+		power = np.abs(np.fft.fft(self.audioBuffer * win)) #abs calculates the complex magnitude, for some reason
 		freqs = np.fft.fftfreq(self.bufferSize, 1/self.sampleRate)
 		freqs = freqs[:int(len(freqs)/2)] #remove symetric negative frequencies
 		power = power[:int(len(power)/2)]
